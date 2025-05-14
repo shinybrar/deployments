789e40e87e860e35dcfbc2bf5db1532ae6d82c7a
Dustin Jenkins <djenkins.cadc@gmail.com>
2025-05-14 13:29:15 -0700

    feat: carta 5 support and enable compression and remove port spec

--- helm/applications/skaha/launch-scripts/skaha-carta.sh
+++ helm/applications/skaha/launch-scripts/skaha-carta.sh
@@ -7,15 +7,21 @@ SELF=skaha-carta
 TS=$(date)
 echo "$TS $SELF START"
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: skaha-carta <root> <folder>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: skaha-carta <root> <folder> <session url>"
     exit 2
 fi
 
 ROOT=$1
 FOLDER=$2
+URL_PREFIX=$3
+COMMAND="carta --no_browser --top_level_folder=${ROOT} --idle_timeout=100000 --debug_no_auth --http_url_prefix=${URL_PREFIX} ${FOLDER}"
 echo "root: $ROOT"
 echo "folder: $FOLDER"
-echo "command: carta --no_browser --top_level_folder=$ROOT --port=6901 --idle_timeout=100000 --debug_no_auth $FOLDER"
-carta --no_browser --top_level_folder=$ROOT --port=6901 --idle_timeout=100000 --debug_no_auth $FOLDER
+echo "url_prefix: $URL_PREFIX"
+echo "command: ${COMMAND}"
+echo "Add --verbosity=5 to the command for more output"
+echo ""
+eval "${COMMAND}"
+
 # A bit over a day timeout. Disable token authentication.
