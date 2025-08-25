81e36a8e70ae8b7a2eae11615b1f00f7f71a4a01
Dustin Jenkins <djenkins.cadc@gmail.com>
2025-08-25 14:18:50 -0700

    fix: debug redis puts

--- helm/applications/skaha/image-cache/cache-images.sh
+++ helm/applications/skaha/image-cache/cache-images.sh
@@ -4,9 +4,11 @@ URL="https://$harborHost/api/v2.0/projects?page_size=100"
 PROJECT_URL="https:/$harborHost/api/v2.0/projects"
 
 # checking if redis instance available or not
-if ! redis-cli -h $REDIS_HOST -p $REDIS_PORT ping > /dev/null 2>&1; then
+if [ -z "$HARBOR_HOST" ]; then
     echo "Redis server is not running. Please start the Redis server and try again."
     exit 1
+else
+    echo "Writing to ${REDIS_HOST}:${REDIS_PORT}"
 fi
 
 temp_key=$(date +%s)
