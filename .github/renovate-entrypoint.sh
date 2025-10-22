4afb08691f720274e78f85de90fe5a85e3590893
Shiny Brar <shiny.brar@nrc-cnrc.gc.ca>
2025-10-22 14:42:35 -0700

    fix(renovate): added fix for the container entrypoint to install uv, go, helm-docs

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -2,13 +2,20 @@
 
 echo "Running Renovate Entrypoint Script"
 
-# uv comes pre-installed on the renovate:full image
+# Install uv
+echo "Installing uv"
+curl -LsSf https://astral.sh/uv/install.sh | sh
+echo "uv Installed: $(which uv)"
+# Install Go
+echo "Installing Go"
+install-tool golang 1.25.3
+echo "Go Installed: $(which go)"
 # Install Helm Docs
 echo "Installing Helm Docs"
 go install github.com/norwoodj/helm-docs/cmd/helm-docs@latest
-echo "Helm Docs Installed: $(which helm-docs): $(helm-docs --version)"
+echo "Helm Docs Installed: $(which helm-docs)"
 
-# Run renovate
+# Run renovate bot
 renovate
 
 echo "Renovate Run Complete"
