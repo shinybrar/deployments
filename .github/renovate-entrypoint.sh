893180418e57e3fad20233ec491d0ca3bb4c7dfb
Shiny Brar <shiny.brar@nrc-cnrc.gc.ca>
2025-10-22 15:54:11 -0700

    fix(renovate): updates

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -6,6 +6,10 @@ echo "Running Renovate Entrypoint Script"
 echo "Installing uv"
 curl -LsSf https://astral.sh/uv/install.sh | sh
 echo "uv Installed: $(which uv)"
+# Install Helm
+echo "Installing Helm"
+install-tool helm 3.19.0
+echo "Helm Installed: $(which helm)"
 # Install Go
 echo "Installing Go"
 install-tool golang 1.25.3
@@ -14,8 +18,6 @@ echo "Go Installed: $(which go)"
 echo "Installing Helm Docs"
 go install github.com/norwoodj/helm-docs/cmd/helm-docs@latest
 echo "Helm Docs Installed: $(which helm-docs)"
-
 # Run renovate bot
 renovate
-
 echo "Renovate Run Complete"
