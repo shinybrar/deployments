dd334674ffa54afae9376781687dea64d60abc46
Shiny Brar <shiny.brar@nrc-cnrc.gc.ca>
2025-10-21 17:28:33 -0700

    fix(pc): lint

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -4,6 +4,9 @@ echo "Running Renovate Entrypoint Script"
 
 # Install uv and sync dependencies
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uv sync
 
-echo "Renovate Entrypoint Script Complete"
+# Run renovate
+renovate
+
+# Debugging
+find / | grep "deployments"
